e3a18a36e37dfdc5a63f6773e9aae5d7fbcbc322
Ridho Hauzan <dotcomhomo@gmail.com>
2025-02-22 13:31:06 +0700

    Implement keyword validation and API submission in homepage component

--- app/page.js
+++ app/page.js
@@ -1,14 +1,40 @@
 "use client";
 
 import { useState } from "react";
-import { Input, Button } from "antd";
+import { Input, Button, message } from "antd";
 import "@/app/globals.css";
+import axios from "axios";
 
 export default function Home() {
   const [keyword, setKeyword] = useState("");
+  const [isLoading, setIsLoading] = useState(false)
+  const [messageApi, contextHolder] = message.useMessage();
+
+  async function clickSubmit () {
+    if (!keyword) {
+      messageApi.open({
+        type: 'error',
+        content: 'Keyword is required',
+      });
+
+      return;
+    }
+
+    setIsLoading(true);
+
+    try {
+      const response = await axios.post("/api/generate", { keyword });
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
 
   return (
     <div className="flex flex-col items-center mt-5 min-h-screen bg-black p-8 gap-8">
+      {contextHolder}
       <h1 className="text-2xl text-white font-bold text-center">
         Scrape eBay Products in Easy Way
       </h1>
@@ -21,7 +47,7 @@ export default function Home() {
         />
         <Button
           className="custom-button px-5 py-2 text-white"
-          onClick={() => alert(`Searching for: ${keyword}`)}
+          onClick={() => clickSubmit()}
         >
           Submit
         </Button>
